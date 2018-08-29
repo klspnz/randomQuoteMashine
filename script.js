1067426373651eaa47f710700c29475a1cee9137
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-08-29 16:36:57 +0200

    napravljen projekat

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
 var dugme = document.querySelector('#dugme');
 var body = document.querySelector('body');
 var text = document.querySelector('.container p');
+var author = document.querySelector('.container span');
 var button = document.querySelector('.container button');
 var colors = ['#d49505', '#0b17b1', '#af1535', '#5b0ccd'];
+var quotes = [
+  {
+    quote: 'Neki text 1',
+    author: 'author 1'
+  },
+  {
+    quote: 'Neki text 2',
+    author: 'author 2'
+  },
+  {
+    quote: 'Neki text 3',
+    author: 'author 3'
+  },
+  {
+    quote: 'Neki text 4',
+    author: 'author 4'
+  },
+  {
+    quote: 'Neki text 5',
+    author: 'author 5'
+  },
+  {
+    quote: 'Neki text 6',
+    author: 'author 6'
+  }
+];
 
 function setColor() {
-  body.style.backgroundColor = 'red';
-  text.style.color = 'red';
-  button.style.backgroundColor = 'red';
+  var index = randomNumber(colors);
+  body.style.backgroundColor = colors[index];
+  text.style.color = colors[index];
+  author.style.color = colors[index];
+  button.style.backgroundColor = colors[index];
 }
 
-dugme.addEventListener('click', setColor);
+function setQuote() {
+  var index = randomNumber(quotes);
+  text.innerText = quotes[index].quote;
+  author.innerText = quotes[index].author;
+}
+
+function randomNumber(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+
+dugme.addEventListener('click', () => {
+  setColor();
+  setQuote();
+});
